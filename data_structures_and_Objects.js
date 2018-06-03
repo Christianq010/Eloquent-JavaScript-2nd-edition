76587d40579689f70c09adfdf5ef747857a4dbec
Christiaan Quyn <christianq010@gmail.com>
2018-06-03 11:49:31 +0530

    add methods for strings content

---
+++ data_structures_and_Objects.js
@@ -0,0 +1,19 @@
+// ------------------------------------
+// Some important things to remember
+// ------------------------------------
+
+// 
+
+// Strings and their properties --------------
+
+// Slice and IndexOf
+console.log("coconuts".slice(4, 7));
+// → nut
+console.log("coconut".indexOf("u"));
+// → 5”
+
+// Trim (removes whitespace)
+console.log("  okay \n ".trim());
+// → okay”
+
+
