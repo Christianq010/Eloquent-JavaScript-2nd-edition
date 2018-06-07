da7ce0549a51d369aac6827e08b60959aefa7611
Christiaan Quyn <christianq010@gmail.com>
2018-06-07 13:12:52 +0530

    format content

--- chapter_4_data_structures_and_Objects.js
+++ chapter_4_data_structures_and_Objects.js
@@ -83,56 +83,56 @@ function reverseArray(array) {
         output.push(array[i]);
     }
     return output;
-  }
-  
-  function reverseArrayInPlace(array) {
+}
+
+function reverseArrayInPlace(array) {
     for (let i = 0; i < Math.floor(array.length / 2); i++) {
-      let old = array[i];
-      array[i] = array[array.length - 1 - i];
-      array[array.length - 1 - i] = old;
+        let old = array[i];
+        array[i] = array[array.length - 1 - i];
+        array[array.length - 1 - i] = old;
     }
     return array;
-  }
-  
-  console.log(reverseArray(["A", "B", "C"]));
-  // → ["C", "B", "A"];
-  let arrayValue = [1, 2, 3, 4, 5];
-  reverseArrayInPlace(arrayValue);
-  console.log(arrayValue);
-  // → [5, 4, 3, 2, 1]
+}
+
+console.log(reverseArray(["A", "B", "C"]));
+// → ["C", "B", "A"];
+let arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
+// → [5, 4, 3, 2, 1]
 
 // A List
 function arrayToList(array) {
     let list = null;
     for (let i = array.length - 1; i >= 0; i--) {
-      list = {value: array[i], rest: list};
+        list = {value: array[i], rest: list};
     }
     return list;
-  }
-  
-  function listToArray(list) {
+}
+
+function listToArray(list) {
     let array = [];
     for (let node = list; node; node = node.rest) {
-      array.push(node.value);
+    array.push(node.value);
     }
     return array;
-  }
-  
-  function prepend(value, list) {
+}
+
+function prepend(value, list) {
     return {value, rest: list};
-  }
-  
-  function nth(list, n) {
+}
+
+function nth(list, n) {
     if (!list) return undefined;
     else if (n == 0) return list.value;
     else return nth(list.rest, n - 1);
-  }
-  
-  console.log(arrayToList([10, 20]));
-  // → {value: 10, rest: {value: 20, rest: null}}
-  console.log(listToArray(arrayToList([10, 20, 30])));
-  // → [10, 20, 30]
-  console.log(prepend(10, prepend(20, null)));
-  // → {value: 10, rest: {value: 20, rest: null}}
-  console.log(nth(arrayToList([10, 20, 30]), 1));
-  // → 20
+}
+
+console.log(arrayToList([10, 20]));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(listToArray(arrayToList([10, 20, 30])));
+// → [10, 20, 30]
+console.log(prepend(10, prepend(20, null)));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(nth(arrayToList([10, 20, 30]), 1));
+// → 20
