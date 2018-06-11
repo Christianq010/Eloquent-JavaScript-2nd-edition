29881f916cdeae216e79991360edf98889a695fc
Christiaan Quyn <christianq010@gmail.com>
2018-06-11 16:19:09 +0530

    add chapter 2 content

--- chapter_2_program_structures.js
+++ chapter_2_program_structures.js
@@ -1,2 +1,18 @@
 // Looping a triangle
+for (let star = '#'; star.length < 8; star += '#') {
+    console.log(star);
+}
+
+// FizzBuzz challenge
+for (let number = 1; number <= 100; number ++) {
+    let output = "";
+    if (number % 3 == 0) {
+        output += "Fizz";
+    } else if (number % 5 == 0){
+        output += "Buzz";
+    }
+    console.log(output || number);
+}
+
+// Chessboard challenge
 
